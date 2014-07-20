7a8d4d586a9f94cf6fc629289c16718b2c22c5b0
Ulrich Kaiser <ulrich.kaiser@hmtm.de>
2014-07-20 21:45:26 +0200

    Add new functionality in elementFactory and tests

--- app/scripts/core/elementFactory.js
+++ app/scripts/core/elementFactory.js
@@ -1,16 +1,34 @@
-define(['uuid'], function (uuid) {
-	'use strict';
+define(['uuid'], function(uuid) {
+  'use strict';
 
-	var createPart = function (categoryId, length) {
-		return {
-			id: uuid.v4(),
-			categoryId: categoryId,
-			length: length
-		};
-	};
+  var createPart = function(categoryId, length) {
+    return {
+      id: uuid.v4(),
+      categoryId: categoryId,
+      length: length
+    };
+  };
 
-	return {
-		createPart: createPart
-	};
+  var createCategory = function(color, name) {
+    return {
+      id: uuid.v4(),
+      color: color,
+      name: name
+    };
+  };
+
+  var createWork = function () {
+    return {
+      id: uuid.v4(),
+      parts: [],
+      categories: []
+    };
+  };
+
+  return {
+    createPart: createPart,
+    createCategory: createCategory,
+    createWork: createWork
+  };
 
 });
--- test/elementFactorySpecs.js
+++ test/elementFactorySpecs.js
@@ -1,20 +1,76 @@
-describe([], function () {
+define([], function() {
+  'use strict';
 
-  var elementFactory;
-  var async = new AsyncSpec(this);
-  
-  async.beforeEach(function (done) {
-    require(['core/elementFactory'], function (_elementFactory) {
-      elementFactory = _elementFactory;
-      done();
+  describe('In elementFactory', function() {
+
+    var elementFactory;
+    var async = new AsyncSpec(this);
+
+    async.beforeEach(function(done) {
+      require(['core/elementFactory'], function(_elementFactory) {
+        elementFactory = _elementFactory;
+        done();
+      });
+    });
+
+    describe('createPart', function () {
+
+      var part;
+      beforeEach(function() {
+        part = elementFactory.createPart('testId', 250);
+      });
+
+      it('should create a new part and set its ID', function() {
+        expect(part.id).toBeDefined();
+      });
+
+      it('should create a new part and set the categoryID', function() {
+        expect(part.categoryId).toBe("testId");
+      });
+
+      it('should create a new part and set the length', function() {
+        expect(part.length).toBe(250);
+      });
+    });
+
+    describe('createCategory', function () {
+
+      var category;
+      beforeEach(function () {
+        category = elementFactory.createCategory("Red", "Bridge");
+      });
+
+      it('should create a new categroy and set its ID', function() {
+        expect(category.id).toBeDefined();
+      });
+
+      it('should create a new categroy and set its color', function () {
+        expect(category.color).toBe("Red");
+      });
+
+      it('should create a new categroy and set its name', function () {
+        expect(category.name).toBe("Bridge");
+      });
+    });
+
+    describe('createWork', function () {
+
+      var work;
+      beforeEach(function () {
+        work = elementFactory.createWork();
+      });
+
+      it('should create a new work and set its ID', function() {
+        expect(work.id).toBeDefined();
+      });
+
+      it('should create a new work and set its parts to an empty array', function() {
+        expect(work.parts).toEqual([]);
+      });
+
+      it('should create a new work and set its categories to an empty array', function() {
+        expect(work.categories).toEqual([]);
+      });
     });
   });
-
-  describe('createPart', function () {
-    it('should create a new part and set its ID', function () {
-      var part = elementFactory.createPart('testId', 250);
-      expect(part.id).toBeDefined();
-    });
-  });
-
 });
