e01ef5b9e5f28f87e28bf4fdba496aec85f8dbfe
Andreas Helmberger <public@ahelmberger.de>
2016-02-08 00:05:25 +0100

    Refactor move-border.js

--- app/bindings/move-border.js
+++ app/bindings/move-border.js
@@ -9,72 +9,82 @@ function registerEventListenerWithTeardown(element, event, handler) {
   ko.utils.domNodeDisposal.addDisposeCallback(element, () => element.removeEventListener(event, handler));
 }
 
-function getAdjacentParts(work, touchOffsetInAvus, touchToleranceInAvus) {
+function getMovementInfo(element, event, work) {
+  const totalWorkLengthInPixels = element.clientWidth;
+  const touchOffsetInPixels = event.clientX - element.offsetLeft;
   const totalParts = work.parts().length;
+  const totalWorkLengthInAvus = work.parts().reduce((sum, currentPart) => sum + currentPart.length(), 0);
+  const avusPerPixel = totalWorkLengthInAvus / totalWorkLengthInPixels;
+  const touchOffsetInAvus = touchOffsetInPixels * avusPerPixel;
+  const touchToleranceInAvus = TOUCH_TOLERANCE_IN_PIXELS * avusPerPixel;
   for (let currentLeftPartIndex = 0, currentBorderOffsetInAvus = 0; currentLeftPartIndex < totalParts - 1; currentLeftPartIndex += 1) {
     const currentLeftPart = work.parts()[currentLeftPartIndex];
     currentBorderOffsetInAvus += currentLeftPart.length();
     const minTouchOffsetInAvus = currentBorderOffsetInAvus - touchToleranceInAvus;
     const maxTouchOffsetInAvus = currentBorderOffsetInAvus + touchToleranceInAvus;
     if (touchOffsetInAvus >= minTouchOffsetInAvus && touchOffsetInAvus <= maxTouchOffsetInAvus) {
-      return { leftIndex: currentLeftPartIndex, rightIndex: currentLeftPartIndex + 1 };
+      return {
+        initialTouchOffsetInAvus: touchOffsetInAvus,
+        leftIndex: currentLeftPartIndex,
+        rightIndex: currentLeftPartIndex + 1,
+        initalLeftPartLengthInAvus: work.parts()[currentLeftPartIndex].length(),
+        initalRightPartLengthInAvus: work.parts()[currentLeftPartIndex + 1].length(),
+        avusPerPixel: avusPerPixel
+      };
     }
   }
   return undefined;
 }
 
+function applyMovementInfo(currentMovementInfo, work, element) {
+  if (!currentMovementInfo) {
+    return;
+  }
+  const minPartLengthInAvus = TOUCH_TOLERANCE_IN_PIXELS * currentMovementInfo.avusPerPixel;
+  const touchOffsetInPixels = event.clientX - element.offsetLeft;
+  const touchOffsetInAvus = touchOffsetInPixels * currentMovementInfo.avusPerPixel;
+  const distanceInAvus = Math.round(touchOffsetInAvus - currentMovementInfo.initialTouchOffsetInAvus);
+  const newLeftPartLength = currentMovementInfo.initalLeftPartLengthInAvus + distanceInAvus;
+  const newRightPartLength = currentMovementInfo.initalRightPartLengthInAvus - distanceInAvus;
+  if (newLeftPartLength >= minPartLengthInAvus && newRightPartLength >= minPartLengthInAvus) {
+    work.parts()[currentMovementInfo.leftIndex].length(newLeftPartLength);
+    work.parts()[currentMovementInfo.rightIndex].length(newRightPartLength);
+  }
+}
+
+function toggleResizeCursor(element, isMoving) {
+  if (isMoving) {
+    element.classList.add('u-col-resize');
+  } else {
+    element.classList.remove('u-col-resize');
+  }
+}
+
 function register() {
   ko.bindingHandlers.moveBorder = {
     init: function (element, valueAccessor) {
-      const value = valueAccessor();
-      const work = value.work;
+      const work = valueAccessor().work;
+
       registerEventListenerWithTeardown(element, 'mousedown', onMouseDown);
       registerEventListenerWithTeardown(element, 'mouseup', onMouseUpOrLeave);
       registerEventListenerWithTeardown(element, 'mouseleave', onMouseUpOrLeave);
       registerEventListenerWithTeardown(element, 'mousemove', onMouseMove);
-      let currentProcess = null;
+
+      let currentMovementInfo = undefined;
+
       function onMouseDown(event) {
-        const totalWorkLengthInPixels = element.clientWidth;
-        const touchOffsetInPixels = event.clientX - element.offsetLeft;
-        const totalParts = work.parts().length;
-        const totalWorkLengthInAvus = work.parts().reduce((sum, currentPart) => sum + currentPart.length(), 0);
-        const avusPerPixel = totalWorkLengthInAvus / totalWorkLengthInPixels;
-        const touchOffsetInAvus = touchOffsetInPixels * avusPerPixel;
-        const touchToleranceInAvus = TOUCH_TOLERANCE_IN_PIXELS * avusPerPixel;
-        const adjacentParts = getAdjacentParts(work, touchOffsetInAvus, touchToleranceInAvus);
-        if (adjacentParts) {
-          currentProcess = {
-            initialTouchOffsetInAvus: touchOffsetInAvus,
-            leftIndex: adjacentParts.leftIndex,
-            rightIndex: adjacentParts.rightIndex,
-            initalLeftPartLengthInAvus: work.parts()[adjacentParts.leftIndex].length(),
-            initalRightPartLengthInAvus: work.parts()[adjacentParts.rightIndex].length(),
-            avusPerPixel: avusPerPixel
-          };
-          console.log(`START: ${JSON.stringify(currentProcess)}`)
-          event.stopPropagation();
-        }
+        currentMovementInfo = getMovementInfo(element, event, work);
+        toggleResizeCursor(element, currentMovementInfo);
       }
+
       function onMouseUpOrLeave(event) {
-        if (currentProcess) {
-          console.log('STOP');
-          // TODO: Übernehmen?
-          currentProcess = null;
-        }
+        currentMovementInfo = undefined;
+        toggleResizeCursor(element, currentMovementInfo);
       }
+
       function onMouseMove(event) {
-        if (currentProcess) {
-          const minPartLengthInAvus = TOUCH_TOLERANCE_IN_PIXELS * currentProcess.avusPerPixel;
-          const touchOffsetInPixels = event.clientX - element.offsetLeft;
-          const touchOffsetInAvus = touchOffsetInPixels * currentProcess.avusPerPixel;
-          const distanceInAvus = Math.round(touchOffsetInAvus - currentProcess.initialTouchOffsetInAvus);
-          const newLeftPartLength = currentProcess.initalLeftPartLengthInAvus + distanceInAvus;
-          const newRightPartLength = currentProcess.initalRightPartLengthInAvus - distanceInAvus;
-          if (newLeftPartLength >= minPartLengthInAvus && newRightPartLength >= minPartLengthInAvus) {
-            work.parts()[currentProcess.leftIndex].length(newLeftPartLength);
-            work.parts()[currentProcess.rightIndex].length(newRightPartLength);
-          }
-        }
+        applyMovementInfo(currentMovementInfo, work, element);
+        toggleResizeCursor(element, currentMovementInfo || getMovementInfo(element, event, work));
       }
     }
   };
