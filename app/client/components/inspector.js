bd26237f4d3d7ea6ff999a8ac14c748249938840
Andreas Helmberger <public@ahelmberger.de>
2016-11-27 23:56:36 +0100

    Make beauty :-)

--- app/client/components/inspector.js
+++ app/client/components/inspector.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const ko = require('knockout');
 
 const template = fs.readFileSync(`${__dirname}/inspector.html`, 'utf8');
@@ -7,6 +8,15 @@ function viewModel(params) {
   const app = params.app;
   return {
     app: app,
+    currentTool: app.currentTool,
+    isExpanded: app.isInspectorExpanded,
+    fileName: ko.pureComputed(() => {
+      if (app.currentPart() && app.currentWork()._.zipFileName()) {
+        return path.basename(app.currentWork()._.zipFileName());
+      } else {
+        return '';
+      }
+    }),
     selectTool: tool => app.currentTool(tool),
     toggleCollapse: () => app.isInspectorExpanded(!app.isInspectorExpanded()),
     currentWork: app.currentWork,
