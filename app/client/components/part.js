0e1028672c40fbd35e37195ca08f3f4e791cb5c4
musikisum <ulrich.kaiser@hmtm.de>
2017-04-23 22:36:30 +0200

    Handle tab and enter input in parts

--- app/client/components/part.js
+++ app/client/components/part.js
@@ -6,15 +6,54 @@ const template = fs.readFileSync(`${__dirname}/part.html`, 'utf8');
 
 function viewModel(params) {
   const app = params.app;
+  const work = params.work;
   const part = params.part;
+
   const vm = {};
+  vm.id = part.id;
   vm.name = part.name;
   vm.color = part.color;
-  vm.backgroundColor = ko.pureComputed(() => utils.getContrastColor(part.color()));
+  vm.contrastColor = ko.pureComputed(() => utils.getContrastColor(part.color()));
   vm.isSelected = ko.pureComputed(() => app.currentPart() === part);
   vm.isInEditMode = ko.observable(false);
   vm.onDblClick = () => vm.isInEditMode(true);
+  vm.onKeyDown = (viewModel, event) => {
+    switch (event.key) {
+      case 'Tab':
+        const offset = event.shiftKey ? -1 : 1;
+        const vmOfNextDomElement = getNeighbouringPartViewModel(work, part, offset);
+        if (vmOfNextDomElement) {
+          vmOfNextDomElement.isInEditMode(true);
+          return false;
+        } else {
+          return true;
+        }
+      case 'Enter':
+        vm.isInEditMode(false);
+        return false;
+      default:
+        return true;
+    }
+  };
+  vm.isInEditMode.subscribe(newValue => {
+    if (!newValue) {
+      vm.name(vm.name().trim());
+      return;
+    }
+    const elem = window.document.querySelector(`[data-part-id='${vm.id()}'] [data-part-name-editor]`);
+    if (elem) {
+      elem.setSelectionRange(0, elem.value.length);
+    }
+  });
   return vm;
+}
+
+function getNeighbouringPartViewModel(work, currentPart, offset) {
+  const index = work.parts().indexOf(currentPart);
+  const neighbour = work.parts()[index + offset];
+  if (!neighbour) return null;
+  const neighbourDomElement = window.document.querySelector(`[data-part-id='${neighbour.id()}']`);
+  return neighbourDomElement ? ko.dataFor(neighbourDomElement) : null;
 }
 
 function register() {
