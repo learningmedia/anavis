5b2bc7a0deafbfcbf8ef579751ec92382311360c
Andreas Helmberger <public@ahelmberger.de>
2018-01-07 21:03:12 +0100

    Get latest release from Github

--- app/client/components/update.js
+++ app/client/components/update.js
@@ -1,34 +1,55 @@
 const fs = require('fs');
 const ko = require('knockout');
-//const { remote } = require('electron');
+const semver = require('semver');
+const { shell } = require('electron');
+const superagent = require('superagent');
+const pkg = require('../../../package.json');
 
 const template = fs.readFileSync(`${__dirname}/update.html`, 'utf8');
 
-function getLatestVersion() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => resolve('v0.6'), 3000);
+const githubReleasePage = 'https://api.github.com/repos/learningmedia/anavis/releases';
 
-  });
+function byVersionDesc(a, b) {
+  return semver.gt(a.tag_name, b.tag_name) ? -1 : 1
+}
+
+function getLatestRelease() {
+  return superagent.get(githubReleasePage)
+    .then(res => {
+      const latest = res.body.sort(byVersionDesc)[0];
+      return {
+        version: semver.valid(latest.tag_name),
+        url: latest.html_url
+      };
+    })
+    .catch(err => {
+      return null;
+    });
 }
 
 
 function viewModel(params) {
   const vm = {};
-  vm.version = ko.observable("");
-  getLatestVersion().then((version) => {
-    vm.version(version);
-  });
+
+  vm.currentVersion = pkg.version;
+  vm.latestRelease = ko.observable(null);
+  vm.updateAvailable = ko.computed(() => vm.latestRelease() && semver.gt(vm.latestRelease().version, vm.currentVersion));
+
   vm.onUpdateClick = function () {
-    console.log("start update");
+    shell.openExternal(vm.latestRelease().url);
+    vm.latestRelease(null);
   };
+
   vm.onCancelClick = function () {
-    vm.version("");
+    vm.latestRelease(null);
   };
+
+  getLatestRelease().then(r => vm.latestRelease(r));
+
   return vm;
 }
 
 function register() {
-  // Register the update component as a new knockout component:
   ko.components.register('av-update', {
     viewModel: viewModel,
     template: template
