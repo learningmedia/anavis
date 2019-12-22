3b68cc28da178db5b51d6a1cbf0335d3d806499c
Andreas Helmberger <public@ahelmberger.de>
2019-12-23 00:31:35 +0100

    Write JSON eject output into file

--- app/server/command-line.js
+++ app/server/command-line.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob');
 const rimraf = require('rimraf');
 const docLoader = require('../shared/doc-loader');
 
@@ -20,24 +23,56 @@ function runCommand({ commandName, args }, cb) {
       break;
     default:
       cb(new Error(`Invalid command name: ${commandName}`));
+      break;
   }
 }
 
 function doEject(args, cb) {
-  if (!args.length) {
-    return cb(new Error('No file name specified'));
+  if (!args.length) return cb(new Error('No file name specified'));
+
+  const directoryMode = args.some(arg => ['-d', '--dir'].includes(arg));
+  const fileOrDirName = args.filter(arg => !['-d', '--dir'].includes(arg))[0];
+
+  if (!fileOrDirName) {
+    return cb(new Error(`Missing ${directoryMode ? 'directory' : 'file'} name`));
   }
 
-  docLoader.openDocument(args[0], (err, doc, unzipDir) => {
-    if (err) return cb(err);
+  const collectFiles = directoryMode
+    ? func => glob(path.join(fileOrDirName, '*.avd'), func)
+    : func => func(null, [fileOrDirName]);
 
-    // eslint-disable-next-line no-console
-    console.log(JSON.stringify(doc, null, 2));
+  collectFiles((err, files) => {
+    if (err) cb(err);
 
-    rimraf(unzipDir, err2 => {
+    if (!files || !files.length) {
+      // eslint-disable-next-line no-console
+      console.log(`No AnaVis files found in directory ${fileOrDirName}`);
+    }
+
+    ejectAvdFiles(files, 0, cb);
+  });
+}
+
+function ejectAvdFiles(avdFileNames, index, cb) {
+  const avdFileName = avdFileNames[index];
+  if (!avdFileName) return cb();
+
+  const jsonFileName = avdFileName + '.json';
+
+  // eslint-disable-next-line no-console
+  console.log(`Ejecting: ${avdFileName} -> ${jsonFileName}`);
+
+  docLoader.openDocument(avdFileName, (err1, doc, unzipDir) => {
+    if (err1) return cb(err1);
+
+    const content = JSON.stringify(doc, null, 2);
+    fs.writeFile(jsonFileName, content, 'utf8', err2 => {
       if (err2) return cb(err2);
-      cb(null);
-    })
+
+      rimraf(unzipDir, err3 => {
+        return err3 ? cb(err3) : ejectAvdFiles(avdFileNames, index + 1, cb);
+      })
+    });
   });
 }
 
