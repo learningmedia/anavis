359c302140322ba27c618dace1190d8224fe43be
Andreas Helmberger <public@ahelmberger.de>
2022-09-30 18:19:06 +0200

    #86 Rewrite document export and add CSV as possible output format

--- README.md
+++ README.md
@@ -6,6 +6,23 @@ AnaVis is a tool to visualize musical form.
 
 ![AnaVis](http://anavis.de/images/documentation/indexabb.png)
 
+## Command line
+
+AnaVis can be used as a command line utility in order to (batch) convert AnaVis files into other formats.
+
+Usage example:
+
+~~~shell
+# Converts the specified files
+anavis eject ./my-file.avd ./my-other-file.avd
+
+# Converts all *.avd files in the specified folder
+anavis eject --directory ./my-folder
+
+# Specify the "format" option as either "json" (default) or "csv"
+anavis eject --directory ./my-folder --format cvs
+~~~
+
 ## License
 
 AnaVis is released under the MIT License. See the bundled LICENSE file for details.
--- app/server/command-line.js
+++ app/server/command-line.js
@@ -2,75 +2,102 @@ const fs = require('fs');
 const path = require('path');
 const glob = require('glob');
 const rimraf = require('rimraf');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
 const docLoader = require('../shared/doc-loader');
+const { stringify: csvStringify } = require('csv-stringify/dist/cjs/sync.cjs');
 
 function checkCommandLine() {
-  const ejectIndex = process.argv.indexOf('eject');
-  if (ejectIndex !== -1) {
-    return {
-      commandName: 'eject',
-      args: process.argv.slice(ejectIndex + 1)
-    };
-  }
+  const cliArgs = yargs(hideBin(process.argv))
+    .command({
+      command: 'eject',
+      describe: 'Export one or multiple AnaVis files to JSON or CSV',
+      builder: yargs => yargs
+        .option('directory', { alias: 'd', demandOption: false, type: 'string' })
+        .option('format', { alias: 'f', demandOption: true, type: 'string', default: 'json' }),
+      handler: argv => {
+        argv.command = 'eject';
+        argv.args = {
+          directory: argv.directory,
+          format: argv.format === 'csv' ? 'csv' : 'json',
+          files: argv._.filter(arg => arg !== 'eject')
+        }
+      }
+    })
+    .help()
+    .argv;
 
-  return null;
+    return cliArgs.command ? cliArgs : null;
 }
 
-function runCommand({ commandName, args }, cb) {
-  switch (commandName) {
+function runCommand(cliArgs, cb) {
+  switch (cliArgs.command) {
     case 'eject':
-      doEject(args, cb);
+      doEject(cliArgs.args, cb);
       break;
     default:
-      cb(new Error(`Invalid command name: ${commandName}`));
+      cb(new Error(`Invalid command name: ${cliArgs.command}`));
       break;
   }
 }
 
 function doEject(args, cb) {
-  if (!args.length) return cb(new Error('No file name specified'));
-
-  const directoryMode = args.some(arg => ['-d', '--dir'].includes(arg));
-  const fileOrDirName = args.filter(arg => !['-d', '--dir'].includes(arg))[0];
-
-  if (!fileOrDirName) {
-    return cb(new Error(`Missing ${directoryMode ? 'directory' : 'file'} name`));
+  if (!args.directory && !args.files) {
+    return cb(new Error('No file name specified'));
   }
 
-  const collectFiles = directoryMode
-    ? func => glob(path.join(fileOrDirName, '*.avd'), func)
-    : func => func(null, [fileOrDirName]);
+  const collectFiles = args.directory
+    ? func => glob(path.join(args.directory, '*.avd'), func)
+    : func => func(null, args.files);
 
   collectFiles((err, files) => {
     if (err) cb(err);
 
     if (!files || !files.length) {
       // eslint-disable-next-line no-console
-      console.log(`No AnaVis files found in directory ${fileOrDirName}`);
+      console.log(`No AnaVis files found in directory ${args.directory}`);
     }
 
-    ejectAvdFiles(files, 0, cb);
+    ejectAvdFiles(files, 0, args.format, cb);
   });
 }
 
-function ejectAvdFiles(avdFileNames, index, cb) {
+function doc2Csv(doc) {
+  const sumPartLengths = parts => parts.reduce((accu, part) => accu + part.length, 0);
+
+  const totalLength = sumPartLengths(doc.parts);
+
+  const segments = doc.parts.map((part, index) => ({
+    startPosition: sumPartLengths(doc.parts.slice(0, index)) / totalLength,
+    title: part.name,
+    color: part.color,
+    text: doc.annotations.map(anno => anno.values[index]).join('\n\n') || ''
+  }));
+
+  return csvStringify(segments, { header: true });
+}
+
+function ejectAvdFiles(avdFileNames, index, format, cb) {
   const avdFileName = avdFileNames[index];
   if (!avdFileName) return cb();
 
-  const jsonFileName = avdFileName + '.json';
+  const outputFileName = `${avdFileName}.${format}`;
 
   // eslint-disable-next-line no-console
-  console.log(`Ejecting: ${avdFileName} -> ${jsonFileName}`);
+  console.log(`Ejecting: ${avdFileName} -> ${outputFileName}`);
 
   docLoader.openDocument(avdFileName, (err1, doc, unzipDir) => {
     if (err1) return cb(err1);
 
-    const content = JSON.stringify(doc, null, 2);
-    fs.writeFile(jsonFileName, content, 'utf8', err2 => {
+    const content = format === 'json'
+      ? JSON.stringify(doc, null, 2)
+      : doc2Csv(doc);
+
+    fs.writeFile(outputFileName, content, 'utf8', err2 => {
       if (err2) return cb(err2);
 
       rimraf(unzipDir, err3 => {
-        return err3 ? cb(err3) : ejectAvdFiles(avdFileNames, index + 1, cb);
+        return err3 ? cb(err3) : ejectAvdFiles(avdFileNames, index + 1, format, cb);
       })
     });
   });
